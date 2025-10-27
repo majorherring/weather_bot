82b002758a629f4602dc45792fe17857f41895e6
Mikhail Ivanov <Brayt2006@gmail.com>
2025-10-27 17:33:43 +0300

    Немного исправил лексикон и добавил работу от /погода

--- handlers/user.py
+++ handlers/user.py
@@ -19,31 +19,6 @@ async def process_help_command(message:Message):
     await message.answer(text=LEXICON_RU["/help"])
 
 
-@user_router.message(Command(commands="weather"))
+@user_router.message(Command(commands=["weather","погода"]))
 async def process_weather_answer(message:Message):
     await message.answer(await get_weather(message.text.lower().split(maxsplit=1)[-1]))
-
-
-
-
-"""
-@user_router.message(
-    F.text.in_([LEXICON_RU["rock"],LEXICON_RU["paper"],LEXICON_RU["scissors"]])
-)
-
-
-async def process_game_button(message:Message):
-    bot_choice=get_bot_choice()
-    await message.answer(text=f"{LEXICON_RU["bot_choice"]} - {LEXICON_RU[bot_choice]}")
-    winner=get_winner(message.text,bot_choice)
-
-    if winner=="user_won":
-        message_effect_id="5046509860389126442"
-    else:
-        message_effect_id=None
-    await message.answer(
-        text=LEXICON_RU[winner],
-        message_effect_id=message_effect_id,
-        reply_markup=yes_no_kb,
-    )
-"""
